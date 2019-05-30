e4d9fd6be51b153bacee04e35f8af98988f960c4
elorenzen <eric.lorenzen@gmail.com>
2019-05-30 10:57:59 -0600

    Created basic email form using Semantic-UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const App = () => {
-    return <div>App component</div>
+    return(
+        <div className="ui raised padded text container segment" style={{marginTop: '10%'}}>
+            <div class="ui equal width form">
+                <div class="fields">
+                    <div class="field">
+                        <label>To</label>
+                        <input type="email" placeholder="Recipient's Email" />
+                    </div>
+                    <div class="field">
+                        <label>From</label>
+                        <input type="email" placeholder="Your Email" />
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Subject</label>
+                    <input type="text" />
+                </div>
+                <div class="field">
+                    <label>Content</label>
+                    <textarea rows="5"></textarea>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default App;
