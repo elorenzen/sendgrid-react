ee15f28160c8bf105f7605f7253e5b4c03012b2b
elorenzen <eric.lorenzen@gmail.com>
2019-05-30 22:07:39 -0600

    Imported Form component into App

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,9 @@
 import React from 'react';
+import Form from './Form';
 
 const App = () => {
     return(
-        <div className="ui raised padded text container segment" style={{marginTop: '10%'}}>
-            <div class="ui equal width form">
-                <div class="fields">
-                    <div class="field">
-                        <label>To</label>
-                        <input type="email" placeholder="Recipient's Email" />
-                    </div>
-                    <div class="field">
-                        <label>From</label>
-                        <input type="email" placeholder="Your Email" />
-                    </div>
-                </div>
-                <div class="field">
-                    <label>Subject</label>
-                    <input type="text" />
-                </div>
-                <div class="field">
-                    <label>Content</label>
-                    <textarea rows="5"></textarea>
-                </div>
-            </div>
-        </div>
+        <Form />
     )
 };
 
